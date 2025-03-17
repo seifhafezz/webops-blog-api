c1562694d728245913a0fac320450672fc730f16
seifhafezz <seif_a.hafez@hotmail.com>
2025-03-17 08:43:03 +0200

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::API
-    include JwtToken
-    def authorize_request  
-        @current_user = current_user  
-    
-        render json: { error: 'Not authorized' }, status: :unauthorized unless @current_user  
-      end  
+  include JwtToken
+  private
+
+  def authorize_request
+    puts "Authorization Header: #{request.headers['Authorization']}" # Debugging
+
+    if decoded_token
+      @current_user = User.find_by(id: decoded_token['user_id'])
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+
+    puts "Decoded Token: #{decoded_token}" # Debugging
+    puts "Current User: #{@current_user.inspect}" # Debugging
+  end
+
+  def current_user
+    @current_user
+  end
 end
--- app/controllers/concerns/jwt_token.rb
+++ app/controllers/concerns/jwt_token.rb
@@ -1,25 +1,27 @@
 module JwtToken  
-    extend ActiveSupport::Concern  
-  
-    SECRET_KEY = Rails.application.credentials.secret_key_base  
-  
-    def encode_token(user_id)  
-      JWT.encode({ user_id: user_id }, SECRET_KEY, 'HS256')  
+  extend ActiveSupport::Concern  
+
+  SECRET_KEY = ENV["SECRET_KEY_BASE"]  || Rails.application.secrets.secret_key_base
+
+  def encode_token(user_id)  
+    JWT.encode({ user_id: user_id }, SECRET_KEY, 'HS256')  
+  end  
+
+  def decoded_token  
+    begin  
+      decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]  
+      puts "Decoded Token Data: #{decoded}" # Debugging
+      decoded
+    rescue JWT::DecodeError  
+      nil  
     end  
-  
-    def decoded_token  
-      begin  
-        JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]  
-      rescue JWT::DecodeError  
-        nil  
-      end  
-    end  
-  
-    def token  
-      request.headers['Authorization'].split(' ')[1]  
-    end  
-  
-    def current_user  
-      User.find(decoded_token['user_id']) if decoded_token  
-    end  
-  end
+  end  
+
+  def token  
+    request.headers['Authorization'].split(' ')[1]  
+  end  
+
+  def current_user  
+    User.find(decoded_token['user_id']) if decoded_token  
+  end  
+end
